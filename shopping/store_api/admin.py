ef7e9dc5cd2fe6a14be0cad8e6fc7e770bc2b862
Kevin Nguyen <xuannamkevin@gmail.com>
2019-06-04 09:42:32 -0400

    refactor: update gitignore

--- shopping/store_api/admin.py
+++ shopping/store_api/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-# admin.site.register(models.StoreUserProfile)
-# # admin.site.register(models.StoreItem)
+admin.site.register(models.StoreUserProfile)
+admin.site.register(models.StoreItem)
